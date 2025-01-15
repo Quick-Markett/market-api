938e2de9e1d94671c5b877873b24e7355d298e49
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-14 21:09:46 -0300

    feat(database): added new connection with postgres database

---
+++ database/database.go
@@ -0,0 +1,55 @@
+package database
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	PostgresInstance *gorm.DB
+)
+
+func setup(db *gorm.DB) {
+	db.AutoMigrate()
+}
+
+func ConnectWithDatabase() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file.")
+	}
+
+	databaseConnection := os.Getenv("DATABASE_URL")
+	if databaseConnection == "" {
+		log.Fatal("Database url is not set in the environment.")
+	}
+
+	PostgresInstance, err = gorm.Open(postgres.New(postgres.Config{
+		DSN: databaseConnection,
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	setup(PostgresInstance)
+
+	database, err := PostgresInstance.DB()
+	if err != nil {
+		log.Fatalf("Failed to get raw SQLDB from GORM: %v", err)
+	}
+
+	database.SetConnMaxIdleTime(15)
+	database.SetMaxIdleConns(10)
+	database.SetConnMaxLifetime(time.Minute * 10)
+
+	if err := database.Ping(); err != nil {
+		log.Fatalf("Error pinging the database: %v", err)
+	} else {
+		log.Println("Database connection established successfully.")
+	}
+}
