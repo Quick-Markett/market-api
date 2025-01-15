1a47b42ad59fd9875330e2188e2adf78ecb23b27
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-14 21:10:58 -0300

    chore(hermes-api): adjusted default project structure

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"main.go/database"
+	"main.go/http/routes"
 )
 
 func main() {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			return strings.HasPrefix(origin, "*")
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	database.ConnectWithDatabase()
+	routes.HandleRequest(r)
+
 	fmt.Println("Iniciando projeto do mercadinho...")
 }
