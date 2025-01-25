be60f83b6afec221287abdf0c2e5e7d671880f7d
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-25 11:40:14 -0300

    fix(models user): replaced password hash by password

--- models/user.go
+++ models/user.go
@@ -3,19 +3,19 @@ package models
 import "time"
 
 type User struct {
-	ID           uint       `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name         string     `json:"name" gorm:"not null"`
-	Email        string     `json:"email" gorm:"not null;unique"`
-	PasswordHash string     `json:"password_hash" gorm:"not null"`
-	PhoneNumber  string     `json:"phone_number"`
-	Address      string     `json:"address"`
-	City         string     `json:"city"`
-	State        string     `json:"state"`
-	ZipCode      string     `json:"zip_code"`
-	CreatedAt    time.Time  `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
-	UpdatedAt    *time.Time `json:"updated_at" gorm:"-"`
-	IsActive     bool       `json:"is_active" gorm:"default:true"`
-	DeletedAt    *time.Time `json:"deleted_at" gorm:"index"`
+	ID          uint       `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name        string     `json:"name" gorm:"not null"`
+	Email       string     `json:"email" gorm:"not null;unique"`
+	Password    string     `json:"password" gorm:"not null"`
+	PhoneNumber string     `json:"phone_number"`
+	Address     string     `json:"address"`
+	City        string     `json:"city"`
+	State       string     `json:"state"`
+	ZipCode     string     `json:"zip_code"`
+	CreatedAt   time.Time  `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt   *time.Time `json:"updated_at" gorm:"-"`
+	IsActive    bool       `json:"is_active" gorm:"default:true"`
+	DeletedAt   *time.Time `json:"deleted_at" gorm:"index"`
 }
 
 func (User) TableName() string {
