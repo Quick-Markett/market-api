b2fc0dfa22b3e103858d12f4c02bb56bb6151f5c
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-26 21:00:35 -0300

    fix(models users): update user model

--- models/user.go
+++ models/user.go
@@ -3,20 +3,21 @@ package models
 import "time"
 
 type User struct {
-	ID          uint       `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name        string     `json:"name" gorm:"not null"`
-	Email       string     `json:"email" gorm:"not null;unique"`
-	Password    string     `json:"password" gorm:"not null"`
-	PhoneNumber string     `json:"phone_number"`
-	Address     string     `json:"address"`
-	City        string     `json:"city"`
-	State       string     `json:"state"`
-	ZipCode     string     `json:"zip_code"`
-	CreatedAt   time.Time  `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
-	UpdatedAt   *time.Time `json:"updated_at" gorm:"-"`
-	IsActive    bool       `json:"is_active" gorm:"default:true"`
-	GoogleId    *string    `json:"google_id" gorm:"uniqueIndex"`
-	DeletedAt   *time.Time `json:"deleted_at" gorm:"index"`
+	ID             uint       `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name           string     `json:"name" gorm:"not null"`
+	Email          string     `json:"email" gorm:"not null;unique"`
+	Password       string     `json:"password" gorm:"not null"`
+	PhoneNumber    string     `json:"phone_number"`
+	Address        string     `json:"address"`
+	City           string     `json:"city"`
+	State          string     `json:"state"`
+	ZipCode        string     `json:"zip_code"`
+	ProfilePicture string     `json:"profile_picture"`
+	CreatedAt      time.Time  `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt      *time.Time `json:"updated_at" gorm:"-"`
+	IsActive       bool       `json:"is_active" gorm:"default:true"`
+	GoogleId       *string    `json:"google_id" gorm:"uniqueIndex"`
+	DeletedAt      *time.Time `json:"deleted_at" gorm:"index"`
 }
 
 func (User) TableName() string {
