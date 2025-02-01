2f02ce052967411f8d99a1f8ebcbb33fd513569e
vitingr <vitorgabrielsbo1460@gmail.com>
2025-02-01 14:14:47 -0300

    fix(models markets): adjusted model

--- models/market.go
+++ models/market.go
@@ -3,20 +3,23 @@ package models
 import "time"
 
 type Market struct {
-	ID          uint       `json:"id" gorm:"primary_key"`
-	Name        string     `json:"name" gorm:"not null"`
-	Email       string     `json:"email" gorm:"not null;unique"`
-	PhoneNumber string     `json:"phone_number"`
-	Address     string     `json:"address"`
-	City        string     `json:"city"`
-	State       string     `json:"state"`
-	ZipCode     string     `json:"zip_code"`
-	Description string     `json:"description"`
-	LogoUrl     string     `json:"logo_url"`
-	CreatedAt   time.Time  `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
-	UpdatedAt   *time.Time `json:"updated_at" gorm:"-"`
-	IsActive    bool       `json:"is_active" gorm:"default:true"`
-	DeletedAt   *time.Time `json:"deleted_at" gorm:"index"`
+	ID              uint       `json:"id" gorm:"primary_key"`
+	Name            string     `json:"name" gorm:"not null"`
+	Email           string     `json:"email" gorm:"not null;unique"`
+	PhoneNumber     string     `json:"phone_number"`
+	Address         string     `json:"address"`
+	City            string     `json:"city"`
+	State           string     `json:"state"`
+	ZipCode         string     `json:"zip_code"`
+	Description     string     `json:"description"`
+	LogoUrl         string     `json:"logo_url"`
+	DeliveryPrice   string     `json:"delivery_price" gorm:"default:0"`
+	DeliveryMinTime float64    `json:"delivery_min_time" gorm:"default:0"`
+	DeliveryMaxTime float64    `json:"delivery_max_time" gorm:"default:30"`
+	CreatedAt       time.Time  `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt       *time.Time `json:"updated_at" gorm:"-"`
+	IsActive        bool       `json:"is_active" gorm:"default:true"`
+	DeletedAt       *time.Time `json:"deleted_at" gorm:"index"`
 }
 
 func (Market) TableName() string {
